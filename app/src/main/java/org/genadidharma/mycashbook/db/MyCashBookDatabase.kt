e75e3603a872c063596415b48fb3afeb7c5b025b
Genadi Dharma <genadislawa@gmail.com>
2022-09-28 10:57:18 +0800

    dao & library fixed, singleton added

--- app/src/main/java/org/genadidharma/mycashbook/db/MyCashBookDatabase.kt
+++ app/src/main/java/org/genadidharma/mycashbook/db/MyCashBookDatabase.kt
@@ -1,10 +1,29 @@
 package org.genadidharma.mycashbook.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.synchronized
 
 @Database(entities = [User::class, Cash::class], version = 1)
 abstract class MyCashBookDatabase: RoomDatabase() {
     abstract fun userDao(): UserDao
     abstract fun cashDao(): CashDao
+
+    companion object {
+        private var INSTANCE: MyCashBookDatabase? = null
+        @InternalCoroutinesApi
+        fun getDatabase(context: Context): MyCashBookDatabase {
+            if (INSTANCE == null) {
+                synchronized(this) {
+                    INSTANCE =
+                        Room.databaseBuilder(context,MyCashBookDatabase::class.java, "mycashbook_db")
+                            .build()
+                }
+            }
+            return INSTANCE!!
+        }
+    }
 }
--- app/src/main/java/org/genadidharma/mycashbook/db/UserDao.kt
+++ app/src/main/java/org/genadidharma/mycashbook/db/UserDao.kt
@@ -1,16 +1,20 @@
 package org.genadidharma.mycashbook.db
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 
 @Dao
 interface UserDao {
+    @Insert
+    fun addUser(user: User)
+
     @Query("SELECT EXISTS(SELECT * FROM user WHERE username = :username AND password = :password)")
-    fun login(username: String, password: String)
+    fun login(username: String, password: String): Boolean
 
     @Query("SELECT EXISTS(SELECT * FROM user WHERE password = :password)")
-    fun checkPassword(password: String)
+    fun checkPassword(password: String): Boolean
 
     @Query("UPDATE user SET password = :newPassword WHERE password = :currentPassword")
     fun changePassword(currentPassword: String, newPassword: String)
